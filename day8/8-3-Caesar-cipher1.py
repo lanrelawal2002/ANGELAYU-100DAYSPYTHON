b3da43ddd7fdf782eea896779c4898f1736a9c98
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-23 00:33:28 +0100

    added the decrypt function to Caesar cipher1.py file

--- day8/8-3-Caesar-cipher1.py
+++ day8/8-3-Caesar-cipher1.py
@@ -6,12 +6,12 @@ def encrypt(plain_text, shift_number):
     for letter in plain_text:
         value = alphabet.index(letter)
         text_index_in_alpha.append(value)
-    print(text_index_in_alpha)
+    #print(text_index_in_alpha)
 
     for number in text_index_in_alpha:
         number += shift_number
         text_index_with_shift.append(number)
-    print(text_index_with_shift)
+    #print(text_index_with_shift)
     
     for digit in text_index_with_shift:
         cipher_letter = alphabet[digit]
@@ -20,6 +20,15 @@ def encrypt(plain_text, shift_number):
     print(f"The encoded text is {encoded_string}.")
     
 
+def decrypt(encrypted_text, shift_number):
+    decoded_string = ""
+    for letter in encrypted_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_number
+        decoded_letter = alphabet[new_position]
+        decoded_string += decoded_letter
+    print(f"The decoded text is {decoded_string}.")
+    
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
@@ -29,4 +38,9 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt(plain_text=text, shift_number=shift)
+if direction == "encode":
+    encrypt(plain_text=text, shift_number=shift)
+elif direction == "decode":
+    decrypt(encrypted_text=text, shift_number=shift)
+else:
+    print(f"...enter either 'encode' or 'decode' to proceed....")
