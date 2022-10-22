3e9e7dfac9465b3b6119020151f22e2d5588cf35
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-23 00:36:59 +0100

    caesarcipher2.py file error corrected

--- day8/8-3-Caesar-cipher2.py
+++ day8/8-3-Caesar-cipher2.py
@@ -10,6 +10,17 @@ def encrypt(user_message, shift_count):
     print(f"The encoded text is {cipher_string}.")
 
 
+def decrypt(encrypted_text, shift_number):
+    decoded_string = ""
+    for letter in encrypted_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_number
+        decoded_letter = alphabet[new_position]
+        decoded_string += decoded_letter
+    print(f"The decoded text is {decoded_string}.")
+    
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -18,4 +29,10 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt(user_message=text, shift_count=shift)
+
+if direction == "encode":
+    encrypt(user_message=text, shift_count=shift)
+elif direction == "decode":
+    decrypt(encrypted_text=text, shift_number=shift)
+else:
+    print(f"...enter either 'encode' or 'decode' to proceed....")
