d81bf8a932906bf27b49ff8fe6721b119010bf04
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-22 20:20:28 +0100

    added new lines to test3.py

---
+++ day8/8-3-Caesar-cipher2.py
@@ -0,0 +1,21 @@
+
+def encrypt(user_message, shift_count):
+    cipher_string = ""
+    for letter in user_message:
+        position = alphabet.index(letter)
+        new_position = position + shift_count
+        cipher_letter = alphabet[new_position]
+        cipher_string += cipher_letter
+
+    print(f"The encoded text is {cipher_string}.")
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+encrypt(user_message=text, shift_count=shift)
--- day8/test3.py
+++ day8/test3.py
@@ -1,13 +1,14 @@
 # print(1 / 2)
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+# 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # text = input("Enter string here: ").lower()
 # text_index_of_alpha = []
 
-
+# use this to test
+# for pylance
 # # print(alphabet.index('h'))
 # # print(alphabet[12])
 # for letter in text:
@@ -16,4 +17,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 # print(text_index_of_alpha)
 
+# print(len(alphabet))
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 print(len(alphabet))
+
+# The length of the alphabet will be printed.
