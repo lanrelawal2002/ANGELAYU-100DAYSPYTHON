2331b7c1a20e846e4464ba115276bf4149c1881a
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-27 23:01:10 +0100

    start code for day9 dictionaries and nesting

---
+++ day9/9-0-start.py
@@ -0,0 +1,25 @@
+
+my_dictionary = {
+    "python": "my very first programming language",
+    "html": "markup language after python",
+    "css": "cascading style sheet"
+}
+
+# print(my_dictionary)
+# print(my_dictionary["python"])
+
+my_dictionary["javascript"] = "my second programming language"
+# print(my_dictionary)
+
+# empty_dictionary = {}
+# empty_dictionary["red"] = "fire, blood or anger"
+# empty_dictionary["blue"] = "sky and water"
+# print(empty_dictionary)
+
+my_dictionary["python"] = "my first OOP language"
+# print(my_dictionary)
+
+for key in my_dictionary:
+    print(key)
+    print(my_dictionary[key])
+# print(my_dictionary)
