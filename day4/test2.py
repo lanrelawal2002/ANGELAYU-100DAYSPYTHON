6a7fcfd6cafb29bcaefe76604f194dae007299b7
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-28 15:54:26 +0100

    added more lists and items

--- day4/test2.py
+++ day4/test2.py
@@ -3,18 +3,21 @@ sports = ["football", "gymnastics", "athletics", "basketball"]
 individuals = ["Neymar Jr.", "Unknown", "Blake", "James"]
 favorites = [countries, sports, individuals]
 
-# print(favorites)
+print(favorites)
+combined_list = ["politics", "economics", countries, "electioneering", "policies"]
+print(combined_list)
 # print(len(sports))
 # print(len(favorites))
 # print(favorites[2])
 
-# print(favorites[1][1])
+print(favorites[1][1])
 # print(favorites[0][1])
-print(countries)
-favorites[0][1] = "Switzerland"
+# print(countries)
+# favorites[0][1] = "Switzerland"
 
 # countries[1] = "Switzerland"
 # sports[1] = "Tennis"
 # individuals[1] = "Federer"
 
-print(countries)
+# print(countries)
+print(combined_list[2][1])
