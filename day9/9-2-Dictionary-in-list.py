723b2038593079af6cc0047c0498bb3b0f1d067b
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-30 08:49:20 +0100

    Angela's code for grading project 9-2

---
+++ day9/9-2-Dictionary-in-list.py
@@ -0,0 +1,27 @@
+# my code for exercise 9-2-Dictionary-in-list
+
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+def add_new_country(first_par, second_par, third_par):
+    new_dictionary = {
+        "country": first_par,
+        "visits": second_par,
+        "cities": third_par,
+    }
+
+    travel_log.append(new_dictionary)
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
