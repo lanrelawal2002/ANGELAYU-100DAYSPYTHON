7afa605bf4a3d581881580961ff031a9468011fc
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-10-30 08:47:18 +0100

    test file for day9 dictionaries and nesting

--- day9/test2.py
+++ day9/test2.py
@@ -1,3 +1,24 @@
 
-for t in range(1):
-    print("Hello")
+# for t in range(1):
+#     print("Hello")
+
+# ***************************************************************
+# professions = ["Doctor", "Lawyer", "Engineer"]
+
+# def edit_professions():
+#     professions.append("Lecturer")
+
+# edit_professions()
+# print(professions)
+
+# ******************************************************************
+
+
+def rules():
+    all_rules = ["Sleep", "Exercise", "Hydrate"]
+    all_rules.append("Pray")
+    print(all_rules)
+    
+rules()
+
+
