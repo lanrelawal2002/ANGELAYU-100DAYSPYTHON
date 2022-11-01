aa21cfe436bc773eea13bf5f04481402e06f7561
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-11-01 21:28:57 +0100

    test file for python dictionaries

--- day9/test.py
+++ day9/test.py
@@ -9,12 +9,73 @@
 # my_library["Thieme"] = "Chirstian Integrity"
 # print(my_library)
 
-operating_systems = {}
 
-for t in range(1):
-    operating_systems["Windows"] = "Bill Gates"
-    operating_systems["Macintosh"] = "Steve Wozniak"
-    operating_systems["Linux"] = "Linus Torvalds"
-    operating_systems["Solaris"] = "Unknown"
+# *************************************************************************
+# operating_system = {}
 
-print(operating_systems)
+# for t in range(1):
+#     operating_systems["Windows"] = "Bill Gates"
+#     operating_systems["Macintosh"] = "Steve Wozniak"
+#     operating_systems["Linux"] = "Linus Torvalds"
+#     operating_systems["Solaris"] = "Unknown"
+
+# print(operating_systems)
+
+
+# *************************************************************************
+
+# all_bid_amount = [18, 205, 9, 36, 74]
+
+# winning_bid = max(all_bid_amount)
+
+# print(winning_bid)
+
+# *************************************************************************
+
+# auction_dictionary = {
+#     "Harry": 18, 
+#     "Frank": 12,
+#     "Anthony": 34,
+#     "Terry": 27,
+# }
+
+# all_bids = []
+
+# for bidder in auction_dictionary:
+#     value = auction_dictionary[bidder]
+#     # print(value)
+#     value += all_bids
+# # print(all_bids)
+
+
+# **************************************************************************
+
+# t = 0
+# all_bids = []
+
+# for v in range(4):
+#     all_bids += 5
+    
+
+# print(all_bids)
+
+# *************************************************************************
+
+
+
+
+# letters = ["a", "b", "c", "d", "e", "f"]
+
+unspc_dict = {
+    "first": "o",
+    "second": "q",
+    "third": "x",
+    "fourth": "i",
+}
+
+value = ""
+
+for position in unspc_dict:
+	value += unspc_dict[position]
+
+print(value)
