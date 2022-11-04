adf0f2ef652180b575618148d87cfe2536050e88
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-11-04 21:13:34 +0100

    my code for days-in-month 10-1

--- day10/10-1-Days-in-month.py
+++ day10/10-1-Days-in-month.py
@@ -12,7 +12,6 @@ def is_leap(year):
     else:
         return "False"
 
-
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     year_type = is_leap(year)
