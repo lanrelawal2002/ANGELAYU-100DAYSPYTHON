0512ad17bbba3ee4e0288531411a6c5e095cc575
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-11-07 00:50:10 +0100

    my code for project 10-2 Calculator

---
+++ day10/10-2-Calculator.py
@@ -0,0 +1,88 @@
+# my code for the project 10-2-Calculator
+# I added the 'exponent' and 'floor division' operations.
+
+from art import logo
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+# Exponent
+def exponent(n1, n2):
+    return n1 ** n2
+
+# Floor division
+def floordiv(n1, n2):
+    return n1 // n2
+
+
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "**": exponent,
+    "//": floordiv,
+}
+
+def casio():
+    print(logo)
+
+    num1 = float(input("What is the first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What is the next number?: "))
+
+    calculator_function = operations[operation_symbol]
+    start_answer = calculator_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {start_answer}")
+
+    # I repeated myself here.....(DRY)
+    # But I was able to restart, continue or exit the calculator.
+    RERUN_CALCULATOR = True
+
+    while RERUN_CALCULATOR:
+        user_rerun = input(f"Type 'y' to continue with {start_answer}, 'r' to restart the calculator, 'e' to exit the calcultor: ").lower()
+        if user_rerun == "r":
+            RERUN_CALCULATOR = False
+            casio()
+        elif user_rerun == 'y':
+            operation_symbol = input("Pick an operation: ")
+            num3 = float(input("What is the next number?: "))
+
+            calculator_function = operations[operation_symbol]
+            end_answer = calculator_function(start_answer, num3)
+            print(f"{start_answer} {operation_symbol} {num3} = {end_answer}")
+            start_answer = end_answer
+        elif user_rerun == "e":
+            print(f".......\nEXITING CALCULATOR")
+            RERUN_CALCULATOR = False
+        else:
+            print(f"Please enter either 'y', 'r' or 'e'")
+
+casio()
+
+
+
+# operation_symbol = input("Pick another operation: ")
+# num3 = int(input("Waht is the next number?: "))
+# calculator_function = operations[operation_symbol]
+# second_answer = calculator_function(start_answer, num3)
+
+# print(f"{start_answer} {operation_symbol} {num3} = {second_answer}")
