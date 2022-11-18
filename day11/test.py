60eda037cf11a6e156799ecf50622226e0622ec4
Lanre Lawal <lanrelawal2002@gmail.com>
2022-11-18 10:50:48 +0100

    test file for day11 blackjack

---
+++ day11/test.py
@@ -0,0 +1,12 @@
+Nested_values = {
+    "Spent": ["50", "81", "12", "13"],
+    "Donors": {
+        "January": "Teachers",
+        "June": "Journalists",
+        "August": "Photographers",
+        "November": "Clerics"
+    },
+    "Exits": 5
+}
+
+print(Nested_values)
