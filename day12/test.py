7f54e50fa975993fb388568b6801cac70d37f40a
Lanre Lawal <lanrelawal2002@gmail.com>
2022-11-30 14:01:03 +0100

    test file for day12 numguess

--- day12/test.py
+++ day12/test.py
@@ -7,5 +7,24 @@ import random
 # number = random.randrange(2)
 # print(number)
 
-score = random.randint(7, 10)
-print(score)
+# score = random.randint(7, 10)
+# print(score)
+
+
+
+def check_value():
+    while value:
+        if score > 1:
+            # value = False
+            return "no coins left"
+        elif score < 1:
+            # value = False
+            return "find coins first"
+
+
+value = True
+score = 11
+
+
+result = check_value()
+print(result)
