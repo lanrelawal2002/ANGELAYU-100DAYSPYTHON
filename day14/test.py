1b1c53630932d59cefe210974426c15c36b4e822
Lanre Lawal <lanrelawal2002@gmail.com>
2022-12-09 11:35:02 +0100

    test file for day14 higherlower

---
+++ day14/test.py
@@ -0,0 +1,56 @@
+
+# numbers = [8, 15, 12, 105, 2, 9, 15, 43, 15]
+# numbers.remove(15)
+# print(numbers)
+
+
+
+# numbers = [8, 15, 12, 105, 2, 9, 15, 43, 15]
+# num_poped = numbers.pop()
+# print("number poped: ", num_poped)
+# print("original list: ", numbers)
+
+
+# colors = ["blue", "green", "yellow", "red", "black", "white"]
+# colors.clear()
+# print(colors)
+
+
+
+def reversing_values(start_num, end_num):
+    empty_list = []
+    counter = 4
+    start_num = counter
+    
+    new_second_number = 39
+   
+
+    return new_second_number
+   
+
+first_number = 44
+second_number = 18
+
+second_number = reversing_values(first_number, second_number)
+print(second_number)
+print(first_number)
+
+
+
+
+
+
+# def reversing_values():
+#     counter = 4
+#     first_number = second_number
+#     new_second_number = 39
+
+#     second_number = new_second_number
+   
+
+# first_number = 44
+# second_number = 18
+
+# reversing_values()
+# print(second_number)
+# print(first_number)
