8225a78f62fdd614f5d237fdaadbf695fa6d199e
Lanre Lawal <lanrelawal2002@gmail.com>
2022-12-21 23:02:18 +0100

    test file for day15 coffeemachine

---
+++ day15/test.py
@@ -0,0 +1,36 @@
+# trial = True
+# score = int(input("Enter a score: "))
+# holder = True
+
+
+# while trial:
+#     while holder:
+#         counter = 0 
+#         if score < 2:
+#             counter += 5
+#             holder = False
+#             trail = False
+#         elif score == 18:
+#             print(f"{score} matches")
+#             holder = False
+#             trial = False
+#         else:
+#             print("exiting now!!!")
+#             holder = False
+#             trial = False
+
+# for r in range(1):
+#     print(f"conter is {counter}.")
+# print("Process terminated")
+
+# debug code above !!!!!!!!!!!!!!!!!!!!!
+# note: error is in the "if" statement
+
+
+total_amount = 10.5
+money_returned = 5.7
+
+if total_amount > money_returned:
+    print(f"all done")
+
+
---
+++ day15/test2.py
@@ -0,0 +1,35 @@
+# print("Please insert coins")
+# quarters = int(input("How many quarters?: "))
+# dimes = int(input("How many dimes?: "))
+# nickels = int(input("How many nickels?: "))
+# pennies = int(input("How many pennies?: "))
+
+# total = ((0.25 * quarters) + (0.10 * dimes) + (0.05 * nickels) + (0.01 * pennies))
+
+# print(total)
+# print(type(total))
+
+
+from math import ceil
+
+
+amount = 9.024993
+amount_rounded = ceil(amount)
+print(amount_rounded)
+
+
+
+
+
+
+
+
+# total_money = 3.0
+# drink_cost = 3.0
+
+# if total_money < drink_cost:
+#     print("moeny paid is less than cost of drink")
+# elif total_money > drink_cost:
+#     print("money paid is greater than cost of drink")
+# else:
+#     print("money paid is equal to cost of drink")
