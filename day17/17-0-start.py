ea09ab66d94072242f0a8ad7dd2bdf7fcc5c8de5
Lanre Lawal <lanrelawal2002@gmail.com>
2023-01-19 17:07:46 +0100

    python class practice

--- day17/17-0-start.py
+++ day17/17-0-start.py
@@ -1,1 +1,13 @@
-# start here
+class User:
+    def __init__(self):
+        print("pulling out a \"BRAND NEW USER\"")
+
+user1 = User()
+user1.id = 198_224
+user1.username = "Jerome"
+print(user1.username)
+
+user2 = User()
+user2.id = 736_050
+user2.username = "Manuel"
+print(user2.id)
