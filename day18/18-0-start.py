5a6d900a74c9d124e2acff66f8ccd7928bb464b4
Lanre Lawal <lanrelawal2002@gmail.com>
2023-02-11 16:41:06 +0100

    python turtle practice

--- day18/18-0-start.py
+++ day18/18-0-start.py
@@ -1,1 +1,20 @@
 # start code for day18 turtle graphics
+
+from turtle import Turtle, Screen
+
+donatello = Turtle()
+donatello.shape("turtle")
+donatello.color("dodgerblue4")
+
+for number in range(4):
+    donatello.right(90)
+    donatello.forward(95)
+# donatello.right(90)
+# donatello.forward(95)
+# donatello.right(90)
+# donatello.forward(95)
+# donatello.right(90)
+# donatello.forward(95)
+
+working_screen = Screen()
+working_screen.exitonclick()
