adbe8fccbbeb1121121568b04186b942ffdd1b52
Lanre Lawal <lanrelawal2002@gmail.com>
2023-02-17 09:49:28 +0100

    angela's random walk code

--- day18/test.py
+++ day18/test.py
@@ -6,23 +6,26 @@ import random
 tim = turtle.Turtle()
 turtle.colormode(255)
 
-def random_colors():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    colors = (r, g, b)
-    return colors
+# def random_colors():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     colors = (r, g, b)
+#     return colors
 
 # colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 directions = [0, 90, 180, 270]
-tim.pensize(15)
+tim.pensize(1)
 tim.speed("fastest")
 
-for _ in range(100):
-    tim.color(random_colors())
+for number in range(10):
+    # tim.color(random_colors())
     tim.forward(30)
     tim.setheading(random.choice(directions))
+
+print(tim.heading())
+print(tim.position())
 
 my_screen = turtle.Screen()
 my_screen.exitonclick()
