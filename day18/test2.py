d7a1a48d74ca91b5e9a028b69833f6aacb0d4e06
Lanre Lawal <lanrelawal2002@gmail.com>
2023-02-18 17:22:35 +0100

    python tuples testing file

---
+++ day18/test2.py
@@ -0,0 +1,10 @@
+# testing ground for tuples
+
+# all_colors = (255, 151, 210)
+# print(all_colors)
+# all_colors[1] = 65   ## tuple object does not support item assignment
+# print(all_colors[0])
+
+
+for digit in range(5):
+    print(digit)
