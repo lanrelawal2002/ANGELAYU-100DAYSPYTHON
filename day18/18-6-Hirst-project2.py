b8d2bc1c7fa4953c33ae56a0479c07c59955b756
Lanre Lawal <lanrelawal2002@gmail.com>
2023-02-20 01:36:53 +0100

    Anglea's version of hirst painting project

---
+++ day18/18-6-Hirst-project2.py
@@ -0,0 +1,26 @@
+# Angela's version of hirst painting project
+
+## RUNNING THE CODE MAY MAKE VSCODE GET STUCK ##
+# leave all COLOR EXTRACTION PORTION of hirst painting project code commented
+
+
+## START COLOR EXTRACTION PORTION OF HIRST PAINTING ##
+# import colorgram
+
+# project_colors = []
+
+# number_of_colors = int(input("How many colors to extract? "))
+# all_colors = colorgram.extract('hirst_image.jpg', number_of_colors)
+
+# for item in all_colors:
+#     r = item.rgb.r
+#     g = item.rgb.g
+#     b = item.rgb.b
+
+#     color_tuple = (r, g, b)
+#     project_colors.append(color_tuple)
+
+# print(project_colors)
+
+
+## END COLOR EXTRACTION PORTION OF HIRST PAINTING ##
