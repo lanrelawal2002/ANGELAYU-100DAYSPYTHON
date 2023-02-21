e3f011d555bf181883280ee0f865f7cfaff4604f
Lanre Lawal <lanrelawal2002@gmail.com>
2023-02-21 14:13:38 +0100

    my version of hirst painting project

--- day18/18-6-Hirst-project.py
+++ day18/18-6-Hirst-project.py
@@ -1,10 +1,10 @@
 # my version of hirst painting project
 
-## RUNNING THE CODE MAY MAKE VSCODE GET STUCK ##
+# RUNNING THE CODE MAY MAKE VSCODE GET STUCK #
 # leave all COLOR EXTRACTION PORTION of hirst painting project code commented
 
 
-## START COLOR EXTRACTION PORTION OF HIRST PAINTING ##
+# START COLOR EXTRACTION PORTION OF HIRST PAINTING #
 # import colorgram
 #
 #
@@ -30,13 +30,50 @@
 # print(colors_list)
 
 
-## END COLOR EXTRACTION PORTION OF HIRST PAINTING ##
+# END COLOR EXTRACTION PORTION OF HIRST PAINTING #
 
 
-project_colors = [(199, 175, 117), (124, 36, 24), (168, 106, 57), (186, 158, 53), (6, 57, 83), (109, 67, 85),
-                  (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47),
-                  (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186),
-                  (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82),
-                  (46, 73, 62), (47, 66, 82)]
+# DRAWING DOT SECTION OF HIRST PAINTING #
+# import turtle
+# import random
 
-print(len(project_colors))
+
+# def turtle_reposition():
+#     """Changes turtle heading to next available lower left position to redo the drawing of dots."""
+#     ninja.setheading(90)
+#     ninja.forward(50)
+#     ninja.setheading(180)
+#     ninja.forward(500)
+#     ninja.setheading(0)
+
+
+# project_colors = [(199, 175, 117), (124, 36, 24), (168, 106, 57), (186, 158, 53), (6, 57, 83), (109, 67, 85),
+#                   (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47),
+#                   (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186),
+#                   (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82),
+#                   (46, 73, 62), (47, 66, 82)]
+
+# ninja = turtle.Turtle()
+# turtle.colormode(255)
+# ninja.penup()
+# ninja.hideturtle()
+# ninja.speed("fastest")
+
+# ninja.setheading(220)
+# ninja.forward(310)
+# ninja.setheading(0)
+
+# for _ in range(10):
+#     for number in range(10):
+#         ninja.dot(20, random.choice(project_colors))
+#         ninja.forward(50)
+
+#     turtle_reposition()
+
+
+# ninja_screen = turtle.Screen()
+# # ninja_screen.screensize(900, 650)
+# ninja_screen.exitonclick()
+
+
+# END DOT SECTION OF HIRST PAINTING #
